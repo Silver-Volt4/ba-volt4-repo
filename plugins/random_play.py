349078c615a898fc26690e979b496e736e7badf5
Silver-Volt4 <35656715+Silver-Volt4@users.noreply.github.com>
2023-05-25 21:33:18 +0200

    add revised plugin

--- plugins/random_play.py
+++ plugins/random_play.py
@@ -1,8 +1,10 @@
 # ba_meta require api 7
-import ba
-import _ba
 from random import choice, randint
-from typing import Any, Sequence
+from typing import Any, Union
+
+# pylint: disable=import-error
+import _ba
+import ba
 from bastd.ui.playlist.browser import PlaylistBrowserWindow
 
 DEFAULT_TEAM_COLORS = ((0.1, 0.25, 1.0), (1.0, 0.25, 0.2))
@@ -11,14 +13,14 @@ DEFAULT_TEAM_NAMES = ("Blue", "Red")
 
 # More or less copied from game code
 # I have no idea what I'm doing here
-class AutoPilotMixin(ba.MultiTeamSession, ba.Session):
+class RandomPlaySessionMixin(ba.MultiTeamSession, ba.Session):
     def __init__(self, playlist) -> None:
         """Set up playlists and launches a ba.Activity to accept joiners."""
         # pylint: disable=cyclic-import
         from bastd.activity.multiteamjoin import MultiTeamJoinActivity
 
         app = _ba.app
-        cfg = app.config
+        _cfg = app.config
 
         super(ba.MultiTeamSession, self).__init__(
             [],
@@ -47,44 +49,60 @@ class AutoPilotMixin(ba.MultiTeamSession, ba.Session):
 
 # Classes for Teams autopilot and FFA autopilot
 # I think they have to be separate in order to comply with `ba.GameActivity.supports_session_type()`
-class APFreeForAllSession(ba.FreeForAllSession, AutoPilotMixin):
+class RandFreeForAllSession(ba.FreeForAllSession, RandomPlaySessionMixin):
     def __init__(self):
-        playlist = APPlaylist(ba.FreeForAllSession)
+        playlist = RandomPlaylist(ba.FreeForAllSession)
         super(ba.FreeForAllSession, self).__init__(playlist)
 
 
-class APDualTeamSession(ba.DualTeamSession, AutoPilotMixin):
+class RandDualTeamSession(ba.DualTeamSession, RandomPlaySessionMixin):
     def __init__(self):
-        playlist = APPlaylist(ba.DualTeamSession)
+        playlist = RandomPlaylist(ba.DualTeamSession)
         super(ba.DualTeamSession, self).__init__(playlist)
 
 
 # The faux playlist that just picks games at random
-class APPlaylist:
+class RandomPlaylist:
     sessiontype: ba.Session
     all_games: list[ba.GameActivity]
     usable_games: list[ba.GameActivity]
+
+    last_game: ba.GameActivity
 
     def __init__(self, sessiontype):
         self.sessiontype = sessiontype
         self.usable_games = [
             gt
-            for gt in APPlaylist.all_games
+            for gt in RandomPlaylist.all_games
             if gt.supports_session_type(self.sessiontype)
         ]
+        self.last_game = None
 
     def pull_next(self) -> dict[str, Any]:
         """
         Generate a new game at random.
         """
-        game = choice(self.usable_games)
-        map = choice(game.get_supported_maps(self.sessiontype))
+
+        has_only_one_game = len(self.usable_games) == 1
+
+        while True:
+            game = choice(self.usable_games)
+            if game == self.last_game:
+                # Don't repeat the same game twice
+                if has_only_one_game:
+                    # ...but don't freeze the game when there's only one game
+                    break
+            else:
+                break
+
+        game_map = choice(game.get_supported_maps(self.sessiontype))
         settings = {
             s.name: s.default for s in game.get_available_settings(self.sessiontype)
         }
-        settings["map"] = map
+        settings["map"] = game_map
 
         if "Epic Mode" in settings:
+            # Throw in an Epic Mode once in a while
             settings["Epic Mode"] = randint(0, 4) == 4
 
         return {"resolved_type": game, "settings": settings}
@@ -100,12 +118,13 @@ def patched__init__(
 ):
     width = 800
     height = 650
-    
+
     ui_scale = ba.app.ui.uiscale
     y_offset = -100 if ui_scale is ba.UIScale.SMALL else 0
     x_offset = 50 if ui_scale is ba.UIScale.SMALL else 0
 
     self.old__init__(sessiontype, transition, origin_widget)
+    # pylint: disable=protected-access
     self._quick_game_button = ba.buttonwidget(
         parent=self._root_widget,
         position=(width - 120 * 2 + x_offset, height - 132 + y_offset),
@@ -113,7 +132,7 @@ def patched__init__(
         size=(120, 60),
         scale=1.1,
         text_scale=1.2,
-        label="AutoPilot",
+        label="Random games",
         on_activate_call=game_starter_factory(sessiontype),
         color=(0.54, 0.52, 0.67),
         textcolor=(0.7, 0.65, 0.7),
@@ -121,34 +140,50 @@ def patched__init__(
 
 
 # Returns a function that starts the game
-def game_starter_factory(sessiontype):
-    session = None
+def game_starter_factory(sessiontype: type[ba.Session]):
+    session: Union[RandFreeForAllSession, RandDualTeamSession] = None
 
     if issubclass(sessiontype, ba.FreeForAllSession):
-        session = APFreeForAllSession
+        session = RandFreeForAllSession
     elif issubclass(sessiontype, ba.DualTeamSession):
-        session = APDualTeamSession
+        session = RandDualTeamSession
     else:
         raise RuntimeError("Can't determine session type")
 
     def on_run():
-        def can_start(game_list):
+        can_start = False
+
+        def do_start(game_list):
+            nonlocal can_start
+            RandomPlaylist.all_games = game_list
+            if not can_start:  # Don't start if the screen fade is still ongoing
+                can_start = True
+            else:
+                start()
+
+        def has_faded():
+            nonlocal can_start
+            if not can_start:  # Don't start if it's still loading
+                can_start = True
+            else:
+                start()
+
+        def start():
             _ba.unlock_all_input()
-            APPlaylist.all_games = game_list
             _ba.new_host_session(session)
 
-        _ba.fade_screen(False, time=0.25)
+        _ba.fade_screen(False, time=0.25, endcall=has_faded)
         _ba.lock_all_input()
-        ba.app.meta.load_exported_classes(ba.GameActivity, can_start)
+        ba.app.meta.load_exported_classes(ba.GameActivity, do_start)
 
     return on_run
 
 
 # ba_meta export plugin
-class AutopilotGames(ba.Plugin):
+class RandomPlayPlugin(ba.Plugin):
     """
-    Plugin that allows you to play FFA and Teams in with random games,
-    eliminating the need to set up playlists to enjoy all BombSquad content.
+    A plugin that allows you to play randomly generated FFA or Teams matches by selecting a random minigame and map for each round. 
+    This eliminates the need to set up long playlists to enjoy all your BombSquad content.
     """
 
     def __init__(self):
